3b611fcb11aff96561cfcba0a4e290f0831fcad5
Hao Wu <echowuhao@gmail.com>
2019-06-16 19:19:15 +0800

    fix

--- snapshottest_ext/tests/test_pandas_format.py
+++ snapshottest_ext/tests/test_pandas_format.py
@@ -3,7 +3,7 @@ from dataframe import PandasSnapshot
 
 
 def test_format(snapshot):
-    df = pd.DataFrame([['x', 'b'], ['c', 'd']],
+    df = pd.DataFrame([['a', 'b'], ['c', 'd']],
                       columns=['col 1', 'col 2'])
     print(df)
     snapshot.assert_match(PandasSnapshot(df))
