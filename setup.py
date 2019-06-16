6f0be50ce4b30a074b8509a239f949ebf7602dcc
Hao Wu <echowuhao@gmail.com>
2019-06-16 16:40:59 +0800

    togithub

--- setup.py
+++ setup.py
@@ -1,5 +1,5 @@
 import pathlib
-from setuptools import setup
+from setuptools import setup, find_packages
 
 # The directory containing this file
 HERE = pathlib.Path(__file__).parent
@@ -9,7 +9,7 @@ README = (HERE / "README.md").read_text()
 
 setup(
     name="snapshottest_ext",
-    version="0.1.0",
+    version="0.2.0",
     description="extra formatter for snapshottest",
     long_description=README,
     long_description_content_type="text/markdown",
@@ -22,7 +22,7 @@ setup(
         "Programming Language :: Python :: 3",
         "Programming Language :: Python :: 3.7",
     ],
-    packages=["snapshottest_ext"],
+    packages=find_packages(exclude=("tests",)),
     include_package_data=True,
     install_requires=["snapshottest"],
 )
