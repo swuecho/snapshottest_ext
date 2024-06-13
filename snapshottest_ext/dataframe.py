e5d149db9f8310e9adad5c50370cb8b44a7d6875
Hao Wu <echowuhao@gmail.com>
2024-06-13 17:38:19 +0800

    parquet

--- snapshottest_ext/dataframe.py
+++ snapshottest_ext/dataframe.py
@@ -1,21 +1,20 @@
+import io
 import pandas as pd
-import pyarrow as pa
 from snapshottest.formatter import Formatter
 from snapshottest.formatters import BaseFormatter
 
 
-class ArrowSerializer:
-
+class ParquetSerializer:
     @staticmethod
     def pandas_to_bytes(df: pd.DataFrame) -> str:
-        df_bytestring = pa.serialize(df).to_buffer().to_pybytes()
+        buffer = io.BytesIO()
+        df.to_parquet(buffer)
+        df_bytestring = buffer.getvalue()
         return df_bytestring
-
     @staticmethod
     def bytes_to_pandas(raw_bytes) -> pd.DataFrame:
-        original_df = pa.deserialize(raw_bytes)
+        original_df = pd.read_parquet(io.BytesIO(raw_bytes))
         return original_df
-
 
 class PandasSnapshot(object):
     def __init__(self, value):
@@ -39,7 +38,7 @@ class PandasFormatter(BaseFormatter):
 
     def store(self, formatter, pandas_snap: PandasSnapshot):
         """ store pd.DataFrame as bytes in snapshot file"""
-        return ArrowSerializer.pandas_to_bytes(pandas_snap.value)
+        return ParquetSerializer.pandas_to_bytes(pandas_snap.value)
 
     #def get_imports(self):
     #    """not useful in this one, because we do not deserialize from the bytes directly"""
@@ -51,7 +50,7 @@ class PandasFormatter(BaseFormatter):
         :param test_value: the value in snapshot.assert_mach(value)
         :param snapshot_value: the value of format.store (after load)
         """
-        prev_df = ArrowSerializer.bytes_to_pandas(snapshot_value)  # deserialize from bytes to pd.DataFrame
+        prev_df = ParquetSerializer.bytes_to_pandas(snapshot_value)  # deserialize from bytes to pd.DataFrame
         pd.testing.assert_frame_equal(test_value.value, prev_df)
 
 
