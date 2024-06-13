f22fc2dc3cf9546a450b8ff862cae9fb579ac45f
Hao Wu <echowuhao@gmail.com>
2024-06-13 17:21:17 +0800

    install snapshottest

--- snapshottest_ext/dataframe.py
+++ snapshottest_ext/dataframe.py
@@ -4,14 +4,17 @@ from snapshottest.formatter import Formatter
 from snapshottest.formatters import BaseFormatter
 
 
-def pandas_to_bytes(df: pd.DataFrame) -> str:
-    df_bytestring = pa.serialize(df).to_buffer().to_pybytes()
-    return df_bytestring
+class ArrowSerializer:
 
+    @staticmethod
+    def pandas_to_bytes(df: pd.DataFrame) -> str:
+        df_bytestring = pa.serialize(df).to_buffer().to_pybytes()
+        return df_bytestring
 
-def bytes_to_pandas(raw_bytes) -> pd.DataFrame:
-    original_df = pa.deserialize(raw_bytes)
-    return original_df
+    @staticmethod
+    def bytes_to_pandas(raw_bytes) -> pd.DataFrame:
+        original_df = pa.deserialize(raw_bytes)
+        return original_df
 
 
 class PandasSnapshot(object):
@@ -36,7 +39,7 @@ class PandasFormatter(BaseFormatter):
 
     def store(self, formatter, pandas_snap: PandasSnapshot):
         """ store pd.DataFrame as bytes in snapshot file"""
-        return pandas_to_bytes(pandas_snap.value)
+        return ArrowSerializer.pandas_to_bytes(pandas_snap.value)
 
     #def get_imports(self):
     #    """not useful in this one, because we do not deserialize from the bytes directly"""
@@ -48,7 +51,7 @@ class PandasFormatter(BaseFormatter):
         :param test_value: the value in snapshot.assert_mach(value)
         :param snapshot_value: the value of format.store (after load)
         """
-        prev_df = bytes_to_pandas(snapshot_value)  # deserialize from bytes to pd.DataFrame
+        prev_df = ArrowSerializer.bytes_to_pandas(snapshot_value)  # deserialize from bytes to pd.DataFrame
         pd.testing.assert_frame_equal(test_value.value, prev_df)
 
 
